e5152a146d3235f863b31713a416b301c486b95d
sabia afrin <sabiaafrinmitisha@gmail.com>
2022-07-03 00:03:26 +0600

    jhon commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const name = 'sabia';
+const lastName = 'afrin';
+let fullName=name+' ' + lastName
+console.log('my full name is fullName :'+ fullName);
+
+
+const website='google' 
+const url = 'https://www.'+website+'.com'
+console.log(url);
+
+
+const country = 'bangladesh '
+const street ='rajshahi'
+let fullMailingAddress;
+console.log('fulMailingAddress:' +country + ' '+ street)
